9a10eddf5e1a0738ccc7ec179570bdb61a9aa98d
Andy Doan <andy@foundries.io>
2018-07-24 14:54:27 -0500

    Add API for listing devices
    Signed-off-by: Andy Doan <andy@foundries.io>

--- ota_api/api/__init__.py
+++ ota_api/api/__init__.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 
 
 def register_blueprints(app):
+    from ota_api.api.device import blueprint as device_blueprint  # NOQA
     for obj in locals().values():
         if isinstance(obj, Blueprint):
             app.register_blueprint(obj)
---
+++ ota_api/api/device.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2018 Foundries.io
+# Author: Andy Doan <andy@foundries.io>
+from flask import Blueprint, current_app, jsonify
+
+blueprint = Blueprint('devices', __name__, url_prefix='/devices')
+
+
+@blueprint.route('/')
+def list():
+    user = current_app.OTAUser()
+    r = jsonify([x for x in user.device_list()])
+    maxd = user.max_devices
+    if maxd != -1:
+        r.headers['X-MAX-DEVICES'] = maxd
+    return r
